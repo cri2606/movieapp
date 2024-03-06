895101ae6d8847e406218c167ecfa6f780079507
cristian <cristiansala894@gmail.com>
2024-03-06 17:14:55 +0100

    aggiornamento

--- js/api/trendingGetters.js
+++ js/api/trendingGetters.js
@@ -9,15 +9,34 @@ const options = {
 };
 
 export const getTrending = async (baseUrl) => {
-    const response = await fetch(baseUrl + "all/day?language=en-US", options);
+    const response = await fetch(baseUrl + "trending/all/day?language=en-US", options);
     const data = await response.json();
     return data;
 }
 
 export const getTrendingMovies = async (baseUrl) => {
-    const response = await fetch(baseUrl + "movie/day?language=en-US", options);
+    const response = await fetch(baseUrl + "trending/movie/day?language=en-US", options);
     const movie = await response.json();
     return movie;
+}
+
+export const getTrendingPeople = async (baseUrl) => {
+    const response = await fetch(baseUrl + "trending/person/day?language=en-US", options);
+    const people = await response.json();
+    return people;
+}
+
+export const getTrendingTV = async (baseUrl) => {
+    const response = await fetch(baseUrl + "trending/tv/day?language=en-US", options);
+    const tv = await response.json();
+    return tv;
+}
+
+let attore = "clooney";
+export const getSearchActor = async (baseUrl) => {
+    const response = await fetch(baseUrl + "search/person" + "?query=" + attore, options);
+    const actor = await response.json();
+    return actor;
 }
 
 /** getTrendingMovies
--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
-import {getTrendingMovies} from "./api/trendingGetters.js";
+import {getTrending, getTrendingMovies, getTrendingPeople, getTrendingTV, getSearchActor} from "./api/trendingGetters.js";
 import {baseUrl} from "./api/config.js";
 
 getTrending(baseUrl);
 getTrendingMovies(baseUrl);
+getTrendingPeople(baseUrl);
+getTrendingTV(baseUrl);
+getSearchActor(baseUrl);
