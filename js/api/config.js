376d8fff7e370f06051a3aa83b9f249fcb531137
cristian <cristiansala894@gmail.com>
2024-03-07 15:18:03 +0100

    gitignore

--- js/api/config.js
+++ js/api/config.js
@@ -4,6 +4,6 @@ export const OPTIONS = {
     //method: "GET",
     headers : {
         //"Content-Type": "application/json",
-        Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzODkwMWIyZDUwMGYxN2ExYTFiNTEyYmQ1NjgwODFkNiIsInN1YiI6IjY1ZTg2YjAxNDJmMTlmMDE2NDhjZjRiYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Ep5yDrqb9B6vcfumVkBsvEKLs3vk58cP0THzjqIbh40"
+        Authorization: "Bearer"
     },
 };
